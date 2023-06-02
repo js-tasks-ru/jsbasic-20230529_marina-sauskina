3f385c5b58bf4b9fc22ed898fe726febeab5fb52
9masha <marina.sauskina@gmail.com>
2023-06-02 16:37:52 +0200

    function that counts factorial of a number given

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
-function factorial(n) {
-  // ваш код...
+function factorial( n ) {
+  // function that counts factorial of a number given(n)
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = n; i > 0; i--) {
+      result = result * i;
+    }
+    return result;
+  }
 }
