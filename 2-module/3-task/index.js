a79719db849986986e5ee163f0cc0ed9c81f3ebc
9masha <marina.sauskina@gmail.com>
2023-06-08 13:41:17 +0200

    object that has 3 methods read / summarise / multiply

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,22 @@
+//object that has 3 methods read / summarise / multiply.
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0,
+
+  //function gets 2 numbers as parameters and rewrites the properties of the object calculator
+  // @ param a,b numbers
+  read: function (a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum: function () {
+    return this.a + this.b;
+  },
+  mul: function () {
+    return this.a * this.b;
+  }
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
